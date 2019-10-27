c4506371d50dad38ae75defbee6aa7089e798219
antoniogdelrio <antoniogdelrio@hotmail.com>
2019-10-27 11:51:31 -0300

    create buttonsReturn function, remove trash code, set the variable scopes with let, add comments and edit the readme file

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-var gameArray, activePlayer, score, bombsSample;
+//instantiating the variables
+let gameArray, activePlayer, score, bombsSample;
 
+//init the game
 gameInit();
 
+//adding the reset button event listener
 document.querySelector('.reset-button').addEventListener('click', function(){
     resetGame();
 })
 
+//adding the buttons events listeners - (1)with bomb, (2)without bomb
 document.getElementById('zone-0-0').addEventListener('click', function(){
     if(activePlayer){
         if(gameArray[0][0]){
@@ -49,6 +53,7 @@ document.getElementById('zone-0-3').addEventListener('click', function(){
         }        
     }
 })
+
 document.getElementById('zone-0-4').addEventListener('click', function(){
     if(activePlayer){
         if(gameArray[0][4]){
@@ -92,6 +97,7 @@ document.getElementById('zone-1-2').addEventListener('click', function(){
         }        
     }
 })
+
 document.getElementById('zone-1-3').addEventListener('click', function(){
     if(activePlayer){
         if(gameArray[1][3]){
@@ -102,6 +108,7 @@ document.getElementById('zone-1-3').addEventListener('click', function(){
         }        
     }
 })
+
 document.getElementById('zone-1-4').addEventListener('click', function(){
     if(activePlayer){
         if(gameArray[1][4]){
@@ -112,6 +119,7 @@ document.getElementById('zone-1-4').addEventListener('click', function(){
         }        
     }
 })
+
 document.getElementById('zone-2-0').addEventListener('click', function(){
     if(activePlayer){
         if(gameArray[2][0]){
@@ -155,6 +163,7 @@ document.getElementById('zone-2-3').addEventListener('click', function(){
         }        
     }
 })
+
 document.getElementById('zone-2-4').addEventListener('click', function(){
     if(activePlayer){
         if(gameArray[2][4]){
@@ -198,6 +207,7 @@ document.getElementById('zone-3-2').addEventListener('click', function(){
         }        
     }
 })
+
 document.getElementById('zone-3-3').addEventListener('click', function(){
     if(activePlayer){
         if(gameArray[3][3]){
@@ -218,6 +228,7 @@ document.getElementById('zone-3-4').addEventListener('click', function(){
         }        
     }
 })
+
 document.getElementById('zone-4-0').addEventListener('click', function(){
     if(activePlayer){
         if(gameArray[4][0]){
@@ -261,6 +272,7 @@ document.getElementById('zone-4-3').addEventListener('click', function(){
         }        
     }
 })
+
 document.getElementById('zone-4-4').addEventListener('click', function(){
     if(activePlayer){
         if(gameArray[4][4]){
@@ -273,52 +285,63 @@ document.getElementById('zone-4-4').addEventListener('click', function(){
 })
 
 function gameInit(){
+    //initial variables config
     activePlayer = 1;
     score = 0;
     bombsSample = 0;
-    gameArray = [[0,0,0,0,0],
-                [0,0,0,0,0], 
-                [0,0,0,0,0],
-                [0,0,0,0,0],
-                [0,0,0,0,0]]
 
+    //creating the game array
+    gameArray = [[0,0,0,0,0],
+                 [0,0,0,0,0], 
+                 [0,0,0,0,0],
+                 [0,0,0,0,0],
+                 [0,0,0,0,0]]
+
+  
+    //initial panel config
     document.querySelector('.panel-score').textContent = 'SCORE: ' + score;
     document.querySelector('.end-message').textContent = 'GOOD LUCKY!';
-    for(var lineIterator=0; lineIterator < gameArray.length ; lineIterator++){
-        for(var arrayIterator = 0; arrayIterator < gameArray[lineIterator].length; arrayIterator++){
-            var randomBomb = Math.floor(Math.random() * 2);
+
+    //fill the field with bombs
+    for(let lineIterator=0; lineIterator < gameArray.length ; lineIterator++){
+        for(let arrayIterator = 0; arrayIterator < gameArray[lineIterator].length; arrayIterator++){
+            let randomBomb = Math.floor(Math.random() * 2);
             gameArray[lineIterator][arrayIterator] = randomBomb;
             if(randomBomb){
                 bombsSample++;
             }
         }
     } 
-    console.log(bombsSample);
-    console.log(gameArray);
 }
 
 function bombHandler(lineIndex, columnIndex){
-    //replace button <-> bomb image
-    var bombImage = document.createElement("img");
+    //replacing button <-> bomb image
+    let bombImage = document.createElement("img");
     bombImage.classList.add('bomb-image');
     bombImage.src = "bomb.jpg";
-    var oldButton = document.querySelector("#zone-button-"+lineIndex+'-'+columnIndex);
+    let oldButton = document.querySelector("#zone-button-"+lineIndex+'-'+columnIndex);
     oldButton.style.display = 'none';
     document.querySelector('#zone-'+lineIndex+'-'+columnIndex).appendChild(bombImage);
-    
+
+    //changing the background of game screen and showing the lose message
     document.querySelector('.game-screen').classList.toggle('game-over');
     document.querySelector('.end-message').textContent = "GAME OVER!"
+
     //ending the game
     activePlayer = 0;
 }
 
 function scoreHandler(lineIndex, columnIndex){
-    var button = document.querySelector('#zone-button-'+lineIndex+'-'+columnIndex);
+    //changing the button style and turn it disabled
+    let button = document.querySelector('#zone-button-'+lineIndex+'-'+columnIndex);
     button.classList.add('no-bomb');
     button.disabled = 'true';
+
+    //updating the score
     score++;
     document.querySelector('.panel-score').textContent = 'SCORE: ' + score;
 
+    //ending the game
     if(score === 25-bombsSample ){
         activePlayer = 0;
         document.querySelector('.end-message').textContent = "YOU WIN THE GAME!"
@@ -326,26 +349,25 @@ function scoreHandler(lineIndex, columnIndex){
 }
 
 function resetGame(){
+    //reseting the game - (1)game over way, (2)during the game way
     if(!activePlayer){
         gameInit();
-        allButtons = document.querySelectorAll('.zone-button');
         document.querySelector('.bomb-image').remove();
-
-        for(var buttonsIterator = 0; buttonsIterator<allButtons.length; buttonsIterator++){
-            allButtons[buttonsIterator].classList.remove('no-bomb');
-            allButtons[buttonsIterator].style.display = '';
-            allButtons[buttonsIterator].removeAttribute('disabled');
-        }
-        
+        buttonsReturn();
         document.querySelector('.game-screen').classList.remove('game-over');
     }
     else if( activePlayer && score>0){
         gameInit();
-        allButtons = document.querySelectorAll('.zone-button');
-        for(var buttonsIterator = 0; buttonsIterator<allButtons.length; buttonsIterator++){
-            allButtons[buttonsIterator].classList.remove('no-bomb');
-            allButtons[buttonsIterator].style.display = '';
-            allButtons[buttonsIterator].removeAttribute('disabled');
-        }
+        buttonsReturn();
+    }
+}
+
+function buttonsReturn(){
+    //returning the initial style of the buttons
+    allButtons = document.querySelectorAll('.zone-button');
+    for(let buttonsIterator = 0; buttonsIterator<allButtons.length; buttonsIterator++){
+        allButtons[buttonsIterator].classList.remove('no-bomb');
+        allButtons[buttonsIterator].style.display = '';
+        allButtons[buttonsIterator].removeAttribute('disabled');
     }
 }
